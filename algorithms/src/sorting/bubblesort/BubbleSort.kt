722f8d17713882a9d4bf156c696e3d7c52f9572e
nicolascberna <nicolascberna@gmail.com>
2025-03-30 23:59:29 -0300

    add bubble sort algorithm

--- algorithms/src/sorting/bubblesort/BubbleSort.kt
+++ algorithms/src/sorting/bubblesort/BubbleSort.kt
@@ -1,3 +1,15 @@
 package sorting.bubblesort
 
-class BubbleSort
+fun bubbleSort(array: IntArray): IntArray {
+    val n = array.size
+    for (i in 0 until n - 1) {
+        for (j in 0 until n - i - 1) {
+            if (array[j] > array[j + 1]) {
+                val temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
+    }
+    return array
+}
