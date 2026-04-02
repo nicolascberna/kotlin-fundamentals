a20bfea0299b7379bb717c6a41c8c812092b69f4
nicolascberna <nicolascberna@gmail.com>
2026-04-02 11:17:22 -0300

    add command template

--- src/main/kotlin/designpatterns/behavioral/command/CommandTemplate.kt
+++ src/main/kotlin/designpatterns/behavioral/command/CommandTemplate.kt
@@ -1,6 +1,43 @@
 package org.example.designpatterns.behavioral.command
 
+import org.junit.Test
+
 interface Command {
     fun execute()
 }
 
+class OrderAddCommand(val id: Int) : Command {
+    override fun execute() {
+        println("Adding order with id $id")
+    }
+}
+
+class OrderPayCommand(val id: Int) : Command {
+    override fun execute() {
+        println("Paying for order with id $id")
+    }
+}
+
+class CommandProcessor {
+    private val queue = arrayListOf<Command>()
+
+    fun addToQueue(command: Command): CommandProcessor = apply {
+        queue.add(command)
+    }
+
+    fun processCommand(): CommandProcessor = apply {
+        queue.forEach { it.execute() }
+        queue.clear()
+    }
+}
+
+class CommandTest {
+    @Test
+    fun testCommand() {
+        CommandProcessor()
+            .addToQueue(OrderAddCommand(1))
+            .addToQueue(OrderAddCommand(2))
+            .addToQueue(OrderPayCommand(1))
+            .processCommand()
+    }
+}
